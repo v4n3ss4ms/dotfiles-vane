30e880e07e4c08a5897e5b267d2e5205d407a7ac
Vanessa <v4n3ss4ms@gmail.com>
2023-05-23 14:24:49 +0200

    remove comments

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,9 +9,3 @@ alias work="cd ~/workspace"
 alias tmp="cd ~/tmp"
 alias webstorm="~/webstorm"
 alias intellij="~/intellij"
-
-# cd() { builtin cd "$@"; ls; }
-# alias ip='ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk "{print $2}"'
-# alias publicIp="curl https://ipinfo.io/ip"
-# trash () { command mv "$@" ~/.Trash; }
-# alias lr='ls -R | grep ":$" | sed -e '\''s/:$//'\'' -e '\''s/[^-][^\/]*\//--/g'\'' -e '\''s/^/   /'\'' -e '\''s/-/|/'\'' | less'
